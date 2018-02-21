b053de29aa29eb2afb0bbe6824f416a82a0042a0
Greg <gtfullton@gmail.com>
2018-02-20 19:35:07 -0500

    Start adding mouth animation

--- public/src/demoscene/girl animate leg.js
+++ public/src/demoscene/girl animate leg.js
@@ -35,6 +35,12 @@ var armLeft;
 var armRight;
 var legLeft;
 var legRight;
+var legRight2;
+var redOvalLong;
+var redOvalArmLeftUpper;
+var redOvalArmLeftUpper;
+var redOvalArmRightUpper;
+var redOvalArmRightUpper;
 
 var girlGroup;
 
@@ -42,7 +48,7 @@ var game = new Phaser.Game(config);
 
 function bootLoader ()
 {
-    this.load.image('loader', 'assets/demoscene/monitor.png');
+    // this.load.image('loader', 'assets/demoscene/monitor.png');
 }
 
 function bootCreate ()
@@ -67,42 +73,42 @@ function preload ()
     this.load.image('arm-right', 'assets/demoscene/arm right.png');
     this.load.image('leg-left', 'assets/demoscene/leg left.png');
     this.load.image('leg-right', 'assets/demoscene/leg right.png');
+    this.load.image('red-oval-long-h', 'assets/demoscene/red-oval-small-h.png');
+    this.load.image('red-oval-short-h', 'assets/demoscene/red-oval-horizontal.png');
+    this.load.image('red-oval-body', 'assets/demoscene/red-oval-vertical.png');
 }
 
 function create ()
 {
-    this.add.image(0, 0, 'bg1').setOrigin(0);
+    // this.add.image(0, 0, 'bg1').setOrigin(0);
 
     this.sound.pauseOnBlur = false;
     voice = this.sound.add('mitekudasai');
     voice.play();
 
-    hairLeft = (this.add.sprite(184, -5, 'hair-left')).setScale(0.18, 0.18).setOrigin(0.5,0).setAngle(8);
-    hairRight = (this.add.sprite(214, 5, 'hair-right')).setScale(0.18, 0.18).setOrigin(0.5,0).setAngle(-8);
-
-    // head is a Sprite at this point vs. just an Image
-    head = (this.add.sprite(204, 83, 'head')).setScale(0.18, 0.18).setOrigin(0.5,0.7);
-    eyes = (this.add.sprite(203, 80, 'eyes')).setScale(0.15, 0.15).setOrigin(0.5,0.7);
-
-    legLeft = (this.add.sprite(198, 160, 'leg-left')).setScale(0.22, 0.22).setOrigin(0.5,0).setAngle(0);
+    var redOvalScale = 0.75;
+    var redOvalArmScale = 0.35
+    redOvalBody = (this.add.sprite(210, 100, 'red-oval-body')).setScale(redOvalScale,redOvalScale).setOrigin(0.5,0).setAngle(0);
+    redOvalArmLeftUpper = (this.add.sprite(150, 154, 'red-oval-long-h')).setScale(redOvalArmScale,redOvalArmScale).setOrigin(0.5,0).setAngle(-45);
+    redOvalArmLeftLower = (this.add.sprite(120, 218, 'red-oval-long-h')).setScale(redOvalArmScale,redOvalArmScale).setOrigin(0.5,0).setAngle(-90);
+    redOvalArmRightUpper = (this.add.sprite(270, 154, 'red-oval-long-h')).setScale(redOvalArmScale,redOvalArmScale).setOrigin(0.5,0).setAngle(45);
+    redOvalArmRightLower = (this.add.sprite(300, 218, 'red-oval-long-h')).setScale(redOvalArmScale,redOvalArmScale).setOrigin(0.5,0).setAngle(90);
+    // var redOvalShort;
+    // var redOvalBody;
+        
     legRight = (this.add.sprite(211, 158, 'leg-right')).setScale(0.22, 0.22).setOrigin(0.5,0).setAngle(0);
+    legRight2 = (this.add.sprite(211, 184, 'leg-right')).setScale(0.22, 0.22).setOrigin(0.5,0.8).setAngle(0);
 
-    girlGroup = this.make.group([{ key: 'shirt', setXY: {x: 215, y: 116}, setScale: {x: 0.26, y:0.26} },
-                                    { key: 'pants', setXY: {x: 208, y: 116}, setScale: {x: 0.22, y:0.22} }]);
-
-    armLeft = (this.add.sprite(170, 60, 'arm-left')).setScale(0.22, 0.22).setOrigin(0.5,0).setAngle(0);
-    armRight = (this.add.sprite(236, 32, 'arm-right')).setScale(0.22, 0.22).setOrigin(0.5,0).setAngle(0);
-                                                                    
 }
 
 function update (time, delta)
 {
-    if (voice.isPlaying) {
-        if (head.rotation > 0.3 || head.rotation < -0.3) {
+    // if (voice.isPlaying) {
+        if (legRight.rotation > 0.3 || legRight.rotation < -0.3) {
             headRotationStep = -1*headRotationStep;
             hairRotationStep = -1*hairRotationStep;
         }
-        Phaser.Actions.Rotate([head, eyes, legRight], headRotationStep);
-        Phaser.Actions.Rotate([ hairLeft, hairRight, legLeft], -1*hairRotationStep);
-    }
+        Phaser.Actions.Rotate([legRight], headRotationStep);
+        Phaser.Actions.Rotate([legRight2], -1*headRotationStep);
+    // }
 }
